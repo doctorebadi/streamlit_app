2471f212eb93059a357b07ac7c3cdff301b475db
Mohsen Ebadi <82760375+doctorebadi@users.noreply.github.com>
2023-01-27 13:25:46 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,3 @@ if uploaded_file is not None:
         st.session_state["preview"] += data[i]
 preview = st.text_area("CSV Preview", "", height=150, key="preview")
 upload_state = st.text_area("Upload State", "", key="upload_state")
-def upload():
-    if uploaded_file is None:
-        st.session_state["upload_state"] = "Upload a file first!"
-    else:
-        data = uploaded_file.getvalue().decode('utf-8')
-        parent_path = pathlib.Path(__file__).parent.parent.resolve()           
-        save_path = os.path.join(parent_path, "data")
-        complete_name = os.path.join(save_path, uploaded_file.name)
-        destination_file = open(complete_name, "w")
-        destination_file.write(data)
-        destination_file.close()
-        st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
-st.button("Upload file to Sandbox", on_click=upload)
